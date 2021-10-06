a42801119b804efdfe35d217e1630adc4016078e
David Wahid <wakhid2802@gmail.com>
2021-10-06 19:27:32 +0500

    - switch - is operator - if conditional

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -6,7 +6,46 @@ namespace lesson2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int? a = null;
+            // string hechnima = string.Empty;
+
+            // if(hechnima is null)
+            // {
+            //     System.Console.WriteLine("Hechnima null ga teng");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine($"Hechnima {hechnima} ga teng");
+            // }
+
+            int son = int.Parse(Console.ReadLine());
+            
+            // switch(son)
+            // {
+            //     case 1: Console.WriteLine("Siz bir kiritdingiz!"); break;
+            //     case 2: Console.WriteLine("Siz ikki kiritdingiz!"); break;
+            //     case 3: Console.WriteLine("Siz uch kiritdingiz!"); break;
+            //     case 4: Console.WriteLine("Siz to'rt kiritdingiz!"); break;
+            //     default: Console.WriteLine($"Siz {son} kiritdingiz!"); break;
+            // }
+
+            // var message = son switch
+            // {
+            //     1 => "Siz bir kiritdingiz!",
+            //     2 => "Siz ikki kiritdingiz!",
+            //     3 => "Siz uch kiritdingiz!",
+            //     4 => "Siz four kiritdingiz!",
+            //     _ => $"Siz {son} kiritdingiz!",
+            // };
+
+            var message = son switch
+            {
+                > 0 => "Siz musbat son kiritdingiz!",
+                (< 0) => "Siz manfiy son kiritdingiz!",
+                _ => "Siz nol kiritdingiz!",
+            };
+
+            Console.WriteLine(message);
         }
     }
 }
