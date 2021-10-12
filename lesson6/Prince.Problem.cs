3fe879217aca65893cd70e24b10c4380aa9bb9b2
David Wahid <wakhid2802@gmail.com>
2021-10-12 19:49:40 +0500

    Kichik shaxzoda

---
+++ lesson6/Prince.Problem.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+
+namespace lesson6
+{
+    public partial class Prince
+    {
+        /// <summary>
+        /// Calculates how many times Prince has to cross planets.
+        /// </summary>
+        /// <returns>The number of times the Prince crosses planets.</returns>
+        public int Travel()
+        {
+            int count = 0;
+            var points = getIntArray();
+
+            Start = new Point(points[0], points[1]);
+            End = new Point(points[2], points[3]);
+
+            var planets = int.Parse(Console.ReadLine());
+            while(planets-->0)
+            {
+                var p = getIntArray();
+                var planet = new Circle(p[2], new Point(p[0], p[1]));
+
+                if(Crosses(planet))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private int[] getIntArray() 
+            =>  Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => int.Parse(p))
+                .ToArray();
+    }
+}
