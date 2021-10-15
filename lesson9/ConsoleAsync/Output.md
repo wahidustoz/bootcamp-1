7dfb3147e5aab04f3d05c24ce674468fcf148c64
David Wahid <wakhid2802@gmail.com>
2021-10-15 21:37:43 +0500

    Task.WhenAll()

---
+++ lesson9/ConsoleAsync/Output.md
@@ -0,0 +1,8 @@
+## Good Morning
+## Frying Egg
+## Making Tea
+## Making Table
+## Table is Ready
+## Tea is Ready
+## Eggs are ready
+## Bon appetito!
--- lesson9/ConsoleAsync/Program.cs
+++ lesson9/ConsoleAsync/Program.cs
@@ -6,6 +6,38 @@ namespace ConsoleAsync
 {
     class Program
     {
+        static async Task MakingTable()
+        {
+            Console.WriteLine($"Making table...");
+            await Task.Delay(3000);
+            Console.WriteLine($"Table is ready!");
+        }
+        static async Task MakingTea()
+        {
+            Console.WriteLine($"Making tea...");
+            await Task.Delay(5000);
+            Console.WriteLine($"Tea is ready!");
+        }
+        static async Task FryingEggs()
+        {
+            Console.WriteLine($"Frying eggs...");
+            await Task.Delay(7000);
+            Console.WriteLine($"Eggs are ready!");
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine($"Good morning");
+
+            var tasks = new Task[] { FryingEggs(), MakingTea(), MakingTable() };
+
+            var all = Task.WhenAll(tasks);
+            await all;
+
+            Console.WriteLine($"Bon appetito!");
+        }
+
+
         static async Task<string> DownloadUserAsync()
         {
             var client = new HttpClient();
@@ -21,7 +53,7 @@ namespace ConsoleAsync
             }
         }
 
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Console.WriteLine($"Boshladi");
             var request = DownloadUserAsync();
