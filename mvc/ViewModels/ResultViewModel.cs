e44073ce9a501592fbbd39c352d57a54255725e8
David Wahid <wakhid2802@gmail.com>
2021-12-06 21:09:57 +0500

    MVC

---
+++ mvc/ViewModels/ResultViewModel.cs
@@ -0,0 +1,10 @@
+namespace mvc.ViewModels;
+
+public class ResultViewModel
+{
+    public string X1 { get; set; }
+    
+    public string X2 { get; set; }
+    
+    public bool HasRoots { get; set; }
+}
